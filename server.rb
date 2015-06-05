823cb3f5cbe1bc4d7a21e1d5c1361d690a86e07d
Melissa Leigh Gore <melissaleighgore@gmail.com>
2015-06-05 17:51:20 -0400

    Refactor loading of users from github api into own method

--- server.rb
+++ server.rb
@@ -3,6 +3,8 @@ require 'sinatra/json'
 require 'httparty'
 require 'pry'
 require 'dotenv'
+require 'pg'
+require_relative 'models/launcher.rb'
 
 Dotenv.load
 
@@ -24,27 +26,38 @@ def github_get(url)
           })
 end
 
-get '/' do
+def load_users
   response = github_get("https://api.github.com/orgs/LaunchAcademy/members")
-  users = {}
+
   response.each do |user|
-    name = user["login"]
-    users[name] = ""
+    @launcher = Launcher.new(user["login"])
+    new_launcher = []
+    new_launcher << @launcher.name
+    sql = "INSERT INTO launchers(name) VALUES($1)"
+    db_connection { |conn| conn.exec_params(sql, new_launcher) }
   end
 
-  users.each do |user|
-    starred_repos = github_get("https://api.github.com/users/#{user[0]}/starred")
-    unless starred_repos.empty?
-      current_user = user[0]
-      user_repo_array = []
-      starred_repos.each do |repo|
-        user_repo_array << repo
-      end
-      users["#{current_user}"] = starred_repos
-    end
-  end
+end
+
+# def get_users
+#
+# end
+
+get '/' do
+  load_users
+  # users.each do |user|
+  #   starred_repos = github_get("https://api.github.com/users/#{user[0]}/starred")
+  #   unless starred_repos.empty?
+  #     current_user = user[0]
+  #     user_repo_array = []
+  #     starred_repos.each do |repo|
+  #       user_repo_array << repo
+  #     end
+  #     users["#{current_user}"] = starred_repos
+  #   end
+  # end
 
 #  launcher = @launcher.all
 
-  erb :index, locals: { users: users }
+  erb :index
 end
