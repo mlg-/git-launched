45d834eef0a20110bfb3b578e23cfc1a9e2e4538
Melissa Leigh Gore <melissaleighgore@gmail.com>
2015-06-06 22:38:23 -0400

    Put requirements into gemfile.

---
+++ Gemfile
@@ -0,0 +1,7 @@
+require 'sinatra'
+require 'sinatra/json'
+require 'httparty'
+require 'pry'
+require 'dotenv'
+require 'pg'
+require_relative 'models/launcher.rb'
--- server.rb
+++ server.rb
@@ -1,11 +1,3 @@
-require 'sinatra'
-require 'sinatra/json'
-require 'httparty'
-require 'pry'
-require 'dotenv'
-require 'pg'
-require_relative 'models/launcher.rb'
-
 Dotenv.load
 
 def db_connection
